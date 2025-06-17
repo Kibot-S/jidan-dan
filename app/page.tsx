f6bd0647c7c8350b27512226b0e68b5e80c3eca7
Kibot-S <shoesdom@gmail.com>
2025-06-17 20:06:30 +0900

    🛠 문구 수정

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         <h1 className="text-4xl font-bold mb-4">✨ 지.단.단 — 지민의 단어 단속 ✨</h1>
         <p className="text-lg">
           상단 메뉴에서 <strong>학습</strong>, <strong>퀴즈</strong>, <strong>보기</strong>를 선택해<br />
-          오늘도 한 가지 더 나아가 보자!
+          오늘도 한 걸음 더 나아가 보자!
         </p>
       </div>
     </main>
